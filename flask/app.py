212289fc2f66bdc5a6d4329cd5928e7c401f0464
Seanlxx256 <seanlxx256@gmail.com>
2024-09-27 00:00:40 -0400

    ex1.3: Add templates directory with base.html for layout and navbar

---
+++ flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='Xiaoxing', current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run(debug=True)
